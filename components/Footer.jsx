092e60d92c4cd8bc5725015c357c6495d3e0e494
mhwebdevelopment <mileshwebdev@gmail.com>
2022-10-17 12:43:22 -0700

    links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import Link from 'next/link';
 import { AiFillFacebook, AiOutlineMail} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
       <p>
-        <a href="https://colescountyanimalshelter.com/"> Home</a> /
-        <a href="https://gofund.me/9542c083"> Donate</a> /
-        <a href="mailto:colescountyanimalshelter1@gmail.com?subject=New Volunteer!&body=I would love to volunteer, could I please have some more info on how the process works?"> Volunteer</a>
+        <Link href="https://colescountyanimalshelter.com/"> Home</Link> /
+        <Link href="https://gofund.me/9542c083"> Donate</Link> /
+        <Link href="mailto:colescountyanimalshelter1@gmail.com?subject=New Volunteer!&body=I would love to volunteer, could I please have some more info on how the process works?"> Volunteer</Link>
       </p>
       <p>
-        <a href="/#about"> About</a> 
-        <a href="/#faq"> Faq&apos;s</a> 
-        <a href="/#animals"> Animals</a>
+        <Link href="/#about"> About</Link> 
+        <Link href="/#faq"> Faq&apos;s</Link> 
+        <Link href="/#animals"> Animals</Link>
       </p><br></br>
       <p>Website by: <a className="websitelink" href="https://www.mhwebdevelopment.com/"> MH Web Development</a></p>
       <p className="icons">
