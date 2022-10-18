6a659aa37fee6bb16e1dacecdafd302b291413e9
mhwebdevelopment <mileshwebdev@gmail.com>
2022-10-17 18:12:52 -0700

    image-small

--- pages/pet/[slug].js
+++ pages/pet/[slug].js
@@ -11,14 +11,14 @@ const PetDetails = ({ pet, pets, bannerData } ) => {
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} className="product-detail-image" />
+            <img src={urlFor(image && image[index])} className="product-detail-image" />
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
               <img 
                 key={i}
                 src={urlFor(item)}
-                className={i === index? 'small-image selected-image' : 'small-image'}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
                 onMouseEnter={() => setIndex(i)}
               />
             ))}
