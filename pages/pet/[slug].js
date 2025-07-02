fdc2ca38fd32362f3c52f62f621d67ea5abd2fe9
mhwebdev <115947469+mhwebdevelopment@users.noreply.github.com>
2025-07-02 14:56:52 -0500

    Update [slug].js

--- pages/pet/[slug].js
+++ pages/pet/[slug].js
@@ -66,7 +66,7 @@ export const getStaticPaths = async () => {
   }));
   return {
     paths,
-    fallback: 'blocking'
+    fallback: 'false'
   }
 }
 export const getStaticProps = async ({ params: { slug }}) => {
